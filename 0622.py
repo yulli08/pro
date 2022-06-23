6414b7e4cf04d7cd8b447d65893ee473a1530666
yulli08 <yulli08@naver.com>
2022-06-23 18:15:04 +0900

    구구단

---
+++ 0622.py
@@ -0,0 +1,15 @@
+for j in range(2, 10):
+    for i in range(1, 10):
+        print('{}x{}={}'.format(j, i, j*i ))
+
+    #2x1=2
+    #2x2=4
+    #. . .
+        #print 중첩을 하기 위해서는 들여쓰기가 두번
+        
+
+    
+
+
+
+
